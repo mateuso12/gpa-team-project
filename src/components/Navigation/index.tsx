6b46141d349f783057effdd0826b4e39b4f132c7
Mateus de Sousa Silva Santos <mateus.santos@xtrategie.com.br>
2022-09-17 19:35:14 -0300

    edit widths from navigation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -28,9 +28,9 @@ const closedMixin = (theme: Theme): CSSObject => ({
     duration: theme.transitions.duration.leavingScreen,
   }),
   overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
+  width: `calc(${theme.spacing(6)} + 1px)`,
   [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
+    width: `calc(${theme.spacing(7)} + 1px)`,
   },
 })
 
